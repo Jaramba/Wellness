cc7a01873404fa560e4c24028f14b730783f5135
Onesmus Mukewa <onesmus.mukewa@hotmail.com>
2012-03-29 13:24:10 +0300

    Minor change before the start of the project

--- conf/deploy.rb
+++ conf/deploy.rb
@@ -1,20 +1,21 @@
-set :application, "inposting"
-set :deploy_to, "~/webapp_releases/inposting"
+
+set :application, "wellington"
+set :deploy_to, "~/webapp_releases/wellington"
 set :shared_children, %w(config lib upload database)
 
-set :repository, "~/hg/repos/inposting"
+set :repository, "~/hg/repos/wellington"
 set :scm, :mercurial
 
-set :user, "ideasafrica"
+set :user, "synacor"
 set :use_sudo, false
-set :python_command, "PYTHONPATH=/home/ideasafrica/webapps/inposting/lib/python2.7:$PYTHONPATH python2.7"
+set :python_command, "PYTHONPATH=/home/synacor/webapps/wellington/lib/python2.7:$PYTHONPATH python2.7"
 
-server "ideasafrica.net", :app, :web, :primary => true
+server "synacor.co.ke", :app, :web, :primary => true
 
 namespace :deploy do
 
   task :restart do
-    run "/home/ideasafrica/webapps/inposting/apache2/bin/restart"
+    run "/home/synacor/webapps/wellington/apache2/bin/restart"
   end
 
   task :finalize_update, :except => { :no_release => true } do
