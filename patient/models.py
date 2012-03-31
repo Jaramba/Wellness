62f7ab05473a4c60f8f0e2c08cd037287bc01a0c
Onesmus Mukewa <onesmus.mukewa@hotmail.com>
2012-04-01 01:04:44 +0300

    added healthprovider model, clean up

--- patient/models.py
+++ patient/models.py
@@ -20,31 +20,36 @@ class Patient(Person):
 	]
 	DRINK_TYPE = [
 	]	
+	EXCERCISE_FREQUENCY=[
+	]
+	DIET_TYPE=[
+	]
+	MEDICATION_TYPE=[]
 
 	next_of_kin = models.CharField(max_length=100, null=True)
-	next_of_kin_phone = models.RegexField(null=True)
+	next_of_kin_phone = models.CharField(max_length=15, null=True)
 	next_of_kin_email = models.EmailField(max_length=120, null=True)
 	next_of_kin_address = models.CharField(max_length=100, null=True)
 	
 	health_provider = models.ForeignKey("healthprovider.HealthProvider")
 	personal_doctor = models.ForeignKey("doctor.Doctor")
 
-	blood_group = models.CharField(choices=BLOOD_GROUPS)
-	weight = models.SmallPositiveIntegerField(default=0)
-	height = models.SmallPositiveIntegerField(default=0)
+	blood_group = models.CharField(max_length=20, choices=BLOOD_GROUPS)
+	weight = models.PositiveSmallIntegerField(default=0)
+	height = models.PositiveSmallIntegerField(default=0)
 	
 	#smokers
 	smoking = models.CharField(max_length=100, choices=SMOKER_TYPE)
 	smoker_type = models.CharField(max_length=100, choices=SMOKE_TYPE,help_text="")
-	duration_smoking = models.SmallPostiveIntegerField(help_text="Number of years smoking")
+	duration_smoking = models.PositiveSmallIntegerField(help_text="Number of years smoking")
 	
 	#drinkers
 	drinking = models.CharField(max_length=100, choices=DRINKER_TYPE)
 	drinker_type = models.CharField(max_length=100, choices=DRINK_TYPE, help_text="")
-	duration_drinking = models.SmallPostiveIntegerField(help_text="Number of years drinking")
+	duration_drinking = models.PositiveSmallIntegerField(help_text="Number of years drinking")
 	
 	#Exercise
-	excercising_times = models.SmallPostiveIntegerField(help_text="Number of years")
+	excercising_times = models.PositiveSmallIntegerField(help_text="Number of years")
 	excercise_frequency = models.CharField(max_length=100, choices=EXCERCISE_FREQUENCY, help_text="")
 	diet = models.CharField(max_length=100, choices=DIET_TYPE, help_text="")
 	medication = models.CharField(max_length=100, choices=MEDICATION_TYPE, help_text="")
