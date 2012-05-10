5031cd31e9f59403dd0f91c8f5affe7256aa6537
Ian Onesmus Mukewa <e-nesmukewa@hotmail.com>
2012-05-10 06:20:21 +0300

    changed the database credentials

--- conf/database_prod.py
+++ conf/database_prod.py
@@ -8,11 +8,11 @@ from settings import os, CURRENT_PATH
 DATABASES = {
     'default': {
         'ENGINE'    : 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME'      : 'ideasafrica_inp', # Or path to database file if using sqlite3.
+        'NAME'      : 'uhai', # Or path to database file if using sqlite3.
         'HOST'      : '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'USER'      : 'ideasafrica_inp',
+        'USER'      : 'uhai',
         'PORT'      : '',                      # Set to empty string for default. Not used with sqlite3.
-        'PASSWORD'  : 'inposting90@210!',
+        'PASSWORD'  : 'uhai90@210!',
  
     }
 }
