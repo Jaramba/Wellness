ed3691ed5ddbd120c20356cce9690d45452b1332
Ian Onesmus Mukewa <e-nesmukewa@hotmail.com>
2012-05-21 17:02:03 +0300

    Ravagegit status

--- conf/database_prod.py
+++ conf/database_prod.py
@@ -8,7 +8,7 @@ from settings import os, CURRENT_PATH
 DATABASES = {
     'default': {
         'ENGINE'    : 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME'      : CURRENT_PATH+'/../db/wellness.db', # Or path to database file if using sqlite3.
+        'NAME'      : '~/webapp_releases/uhai/shared/database/wellness.db', # Or path to database file if using sqlite3.
         'HOST'      : '',                      # Set to empty string for localhost. Not used with sqlite3.
         'USER'      : '',
         'PORT'      : '',                      # Set to empty string for default. Not used with sqlite3.
