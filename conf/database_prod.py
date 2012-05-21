b28ab074fc62f0d32f3cc9ba77393e6258c3e82a
Ian Onesmus Mukewa <e-nesmukewa@hotmail.com>
2012-05-21 17:10:07 +0300

    desperate

--- conf/database_prod.py
+++ conf/database_prod.py
@@ -2,13 +2,11 @@
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 ###########################################################################
-from settings import os, CURRENT_PATH  
-###########################################################################
 
 DATABASES = {
     'default': {
         'ENGINE'    : 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME'      : '~/webapp_releases/uhai/shared/database/wellness.db', # Or path to database file if using sqlite3.
+        'NAME'      : '/home/uhai/webapp_releases/uhai/shared/database/wellness.db', # Or path to database file if using sqlite3.
         'HOST'      : '',                      # Set to empty string for localhost. Not used with sqlite3.
         'USER'      : '',
         'PORT'      : '',                      # Set to empty string for default. Not used with sqlite3.
