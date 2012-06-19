b6e6fc1ab1724ee72b4508f0a7f93690f22aec0e
Onesmus Mukewa <onesmus.mukewa@hotmail.com>
2012-06-19 18:00:47 +0300

    added the Calendar into the ish, looking to see what next i can do

--- wellness/reminders/views.py
+++ wellness/reminders/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+
+from django.shortcuts import render_to_response
+
+@login_required
+def calendar(request, template_name="reminders/calendar.html"):
+	return render_to_response(template_name, {}, context_instance=RequestContext(request))
