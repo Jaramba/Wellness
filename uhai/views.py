bd2bd3c03743016936c09d7916e310983604584a
Onesmus Mukewa <onesmus.mukewa@hotmail.com>
2012-07-08 17:20:52 +0300

    minor bug fix

--- uhai/views.py
+++ uhai/views.py
@@ -13,17 +13,17 @@ from uhai.records.models import Diagnosis, TrackingRecord, TrackingField, Tracki
 from datetime import datetime
 
 def index(request, template_name="", data={}):
-	if not request.session.get('use_page_as'):
-		request.session['use_page_as'] = slugify(request.user.profile.main_role)
 	if request.user.is_authenticated():
+		if not request.session.get('use_page_as'):
+		request.session['use_page_as'] = slugify(request.user.profile.main_role)
 		template_name = 'core/dashboard.html'
-		
+
 		diagnoses = Diagnosis.objects.filter(approved=True, encounter__user=request.user.patient)		
 		tracking_records = list(TrackingRecord.objects.filter(diagnosis__id__in=[diagnosis.problem.id for diagnosis in diagnoses]))
 		vitals = list(TrackingRecord.objects.filter(slug__in=['blood-pressure', 'resting-heart-rate', 'temperature-oral']))
 		tracking_records += vitals
 		tracking_records = set(tracking_records)
-		
+
 		def getitems(tracking_record, ds={}):			
 			tfq = tracking_record.trackingfield_set.all()		
 			teq = TrackingEntry.objects.filter(field__in=TrackingField.objects.filter(tracking_record=tracking_record)).order_by('date_updated')
@@ -43,7 +43,7 @@ def index(request, template_name="", data={}):
 				"haxis": 'Time',
 				"data" : json.dumps(list(_tuple[1]))
 			}
-		
+
 		data['charts'] = map(getchartdata, [(t, getitems(t)) for t in tracking_records])
 		data['items'] = tracking_records
 	else:
