d1b254b00c14b25b44bb741250e650e40270602c
Onesmus Mukewa <onesmus.mukewa@hotmail.com>
2012-10-25 22:55:40 +0300

    secure pages cleanup

--- uhai/portal/secure/urls.py
+++ uhai/portal/secure/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls.defaults import * 
-from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'', include(admin.site.urls)),
-    url(r'^login/$', 'uhai.userprofile.views.login', {
+    url(r'^login/?$', 'uhai.userprofile.views.login', {
     		'template_name':'userprofile/login.html'
     	}, name='login'
     ),
-    url(r'^logout/$','uhai.userprofile.views.logout', {
+    url(r'^logout/?$','uhai.userprofile.views.logout', {
     		'redirect_field_name':'next',
     		'template_name':'userprofile/logout.html'
     	}, name='logout'
