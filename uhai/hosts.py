117d7fbcdbc6d2882d64b413b068f7b56d1bcb05
Onesmus Mukewa <onesmus.mukewa@hotmail.com>
2012-10-25 09:25:17 +0300

    hosts patch

--- uhai/hosts.py
+++ uhai/hosts.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 
 host_patterns = patterns('',
 	host(r'www', settings.ROOT_URLCONF, name='default'),
-    host(r'admin', admin.sites.urls, name='admin'),
     host(r'my', 'uhai.portal.patient.urls', name='my-portal'),#patient portal
     host(r'doctors', 'uhai.portal.providers.urls', name='doctors'),#doctors portal
     host(r'manage', 'uhai.portal.manage.urls', name='myportal'),#insurance management portal
