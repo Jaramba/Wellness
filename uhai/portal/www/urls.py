2c3b9c48aed78f1a381fb63b1861168d5d180018
Onesmus Mukewa <onesmus.mukewa@hotmail.com>
2012-10-26 21:12:37 +0300

    clean!!

--- uhai/portal/www/urls.py
+++ uhai/portal/www/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
-	url(r'^$', 'uhai.views.index', name="index"),	
+	url(r'^$', 'uhai.portal.www.views.index', name="index"),	
     url(r'^about/$', TemplateView.as_view(template_name='website/how_it_works.html'), name="about"),
     url(r'^terms-of-service/$', TemplateView.as_view(template_name='website/terms-conditions.html'), name="terms_and_conditions"),
     url(r'^privacy-policy/$', TemplateView.as_view(template_name='website/privacy_policy.html'), name="privacy-policy"),
