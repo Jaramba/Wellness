0a2433c5622c58a656aff5b6cd64ee289939bfe6
Onesmus Mukewa <onesmus.mukewa@hotmail.com>
2012-10-28 08:21:57 +0300

    fixed minor bug

--- uhai/conf/environment/staging.py
+++ uhai/conf/environment/staging.py
@@ -9,7 +9,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(CURRENT_PATH, '..','templates'),
+    os.path.join(settings.CURRENT_PATH, '..','templates'),
 )
 
 settings.INSTALLED_APPS += [
