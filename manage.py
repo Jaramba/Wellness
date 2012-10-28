79ddee57e878b7e8e2b4ccae454b2179348614f8
Onesmus Mukewa <onesmus.mukewa@hotmail.com>
2012-10-28 13:59:03 +0300

    sweet!

--- manage.py
+++ manage.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
-
-STAGE = os.environ.get('STAGE', 'staging')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'uhai.conf.environment.%s' % STAGE
+from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ['DJANGO_SETTINGS_MODULE'])
-	
-    from django.core.management import execute_from_command_line
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'uhai.conf.environment.%s' % os.environ.get('STAGE', 'staging'))
     execute_from_command_line(sys.argv)
