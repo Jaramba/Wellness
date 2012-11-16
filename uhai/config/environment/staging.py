a6b8ef1ced8a8c07cfef53f9ca4a6bb6da3dc698
Onesmus Mukewa <onesmus.mukewa@hotmail.com>
2012-11-16 13:47:53 +0300

    fix

--- uhai/config/environment/staging.py
+++ uhai/config/environment/staging.py
@@ -65,7 +65,7 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH,'media')
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, '..', 'static_root')
+STATIC_ROOT = os.path.join(PROJECT_PATH, '..', 'static')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
